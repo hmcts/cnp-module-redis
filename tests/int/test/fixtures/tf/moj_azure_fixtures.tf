28fe73ccecc7c359db95659c53e84951e440f684
Calin Marina <calin.marina@gmail.com>
2017-08-29 23:30:44 +0100

    configuration rechecked, fixtures fixed and hopefully subnetid working now

--- tests/int/test/fixtures/tf/moj_azure_fixtures.tf
+++ tests/int/test/fixtures/tf/moj_azure_fixtures.tf
@@ -14,15 +14,6 @@ variable "env" {
   default = "int"
 }
 
-variable "app_settings" {
-  type = "map"
-
-  default = {
-    TEST_SETTING1 = "Setting1"
-    TEST_SETTING2 = "Setting2"
-  }
-}
-
 data "terraform_remote_state" "core_sandbox_infrastructure" {
   backend = "azure"
 
@@ -31,19 +22,33 @@ data "terraform_remote_state" "core_sandbox_infrastructure" {
     storage_account_name = "continomojtfstate"
     container_name       = "contino-moj-tfstate-container"
     key                  = "sandbox-core-infra/dev/terraform.tfstate"
-    subnetid             = "subnet_1"
+    subnetid             = "sandbox-core-infra-subnet-0-dev"
   }
-}
-
-module "redis-cache" {}
-
-module "redis-paas" {
-  source   = "../../../../../"
-  product  = "${var.random_name}-redis-paas"
-  location = "${var.location}"
-  env      = "${var.env}"
 }
 
 output "random_name" {
   value = "${var.random_name}"
+}
+
+resource "azurerm_resource_group" "cache-resourcegroup" {
+  name     = "${var.random_name}-cache-${var.env}"
+  location = "${var.location}"
+}
+
+data "template_file" "redistemplate" {
+  template = "${file("${path.module}/templates/redis-paas.json")}"
+}
+
+resource "azurerm_template_deployment" "redis-paas" {
+  template_body       = "${data.template_file.redistemplate.rendered}"
+  name                = "${var.random_name}-${var.env}"
+  resource_group_name = "${azurerm_resource_group.cache-resourcegroup.name}"
+  deployment_mode     = "Incremental"
+
+  parameters = {
+    cachename = "${var.random_name}-${var.env}"
+    location  = "${azurerm_resource_group.cache-resourcegroup.location}"
+    subnetid  = "${data.terraform_remote_state.core_sandbox_infrastructure.config.subnetid}"
+    env       = "${var.env}"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -10,12 +10,11 @@ variable "env" {
   type = "string"
 }
 
+variable "subnetid" {
+  type = "string"
+}
+
 variable "redis_port" {
   type    = "string"
   default = "6379"
-}
-
-variable "subnetid" {
-  type    = "string"
-  default = "${data.terraform_remote_state.core_apps_infrastructure.subnet_ids[2]}"
 }
