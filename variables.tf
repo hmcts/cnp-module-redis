908f8c36b546b444c7a07e11ec2b71d1718678c5
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-06-16 13:13:18 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "name" {
   type        = string
   default     = null
-  description = "Override the default name of `$product-$component`, useful when upgrading versions"
+  description = "Override the default name of `$product-$env`, useful when upgrading versions"
 }
 
 variable "product" {
